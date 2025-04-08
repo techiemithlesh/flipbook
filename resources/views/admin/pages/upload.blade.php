ae8358505cac4cbdc0523968edca0bdf9f79d24b
Mithlesh Kumar <techie.mithlesh@gmail.com>
2025-04-08 09:57:20 +0530

    Flipbook Admin panel setup

---
+++ resources/views/admin/pages/upload.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layout.index')
+
+@section('title')
+<title>Flipbook Manager - Upload</title>
+@endsection
+
+@section('content')
+    
+@endsection
---
+++ resources/views/components/dropdown-link.blade.php
@@ -0,0 +1,1 @@
+<a {{ $attributes->merge(['class' => 'btn btn-primary transition duration-150 ease-in-out ']) }}>{{ $slot }}</a>
